277f651762933035e01d6b2d7536a8ee13c058c2
clandrew <cmlandrews@gmail.com>
2026-08-01 11:45:57 -0400

    Cleanup, loads 2 images and you can press the button to cycle them

--- rpimageload.ino
+++ rpimageload.ino
@@ -1,29 +1,16 @@
 #include "DEV_Config.h"
 #include "LCD_1in69.h"
 #include "test_image_1.data.h"
-
-// The color format is RGB565. bbbb bxxx xxxx xxxx
-#define WHITE       0xFFFF
-#define RED         0x07E0
-#define GREEN       0x001F
-#define BLUE        0xF800
+#include "test_image_2.data.h"
 
 #define BUTTON1_PIN 14
-#define BUTTON2_PIN 15
-#define BUTTON3_PIN 16
 
 #define LCD_QTR_WIDTH  60
 #define LCD_QTR_HEIGHT 70
 UWORD gMiniStaging[LCD_QTR_WIDTH * LCD_QTR_HEIGHT]; // 1/4 size. To be scaled up at the very end.
-
 UWORD gStaging[LCD_1IN69_WIDTH * LCD_1IN69_HEIGHT ];
-
-const int ballDiam = 50;
-int ballX = 50;
-int ballY = 50;
-int ballVX = 5;
-int ballVY = 5;
-int ballColorIndex = 0;
+int gImageIndex=0;
+bool gButtonPress=false;
 
 void InitializeButton(uint gpio)
 {
@@ -39,52 +26,6 @@ void setup()
   LCD_1IN69_Init();
 
   InitializeButton(BUTTON1_PIN);
-  InitializeButton(BUTTON2_PIN);
-  InitializeButton(BUTTON3_PIN);
-}
-
-static const UWORD colors[] = {RED, GREEN, BLUE };
-void IncrementBallColor()
-{
-  ballColorIndex = (ballColorIndex + 1) % count_of(colors);
-}
-
-UWORD GetBallColor()
-{
-  return colors[ballColorIndex];
-}
-
-void MoveBall()
-{
-  // Horizontal
-  ballX += ballVX;
-  if (ballX <= 0)
-  {
-    ballX = 0;
-    ballVX = -ballVX;
-    IncrementBallColor();
-  }
-  if (ballX > LCD_1IN69_WIDTH-ballDiam-1)
-  {
-    ballX = LCD_1IN69_WIDTH-ballDiam-1;
-    ballVX = -ballVX;
-    IncrementBallColor();
-  }
-  
-  // Vertical
-  ballY += ballVY;
-  if (ballY <= 0)
-  {
-    ballY = 0;
-    ballVY = -ballVY;
-    IncrementBallColor();
-  }
-  if (ballY > LCD_1IN69_HEIGHT-ballDiam-1)
-  {
-    ballY = LCD_1IN69_HEIGHT-ballDiam-1;
-    ballVY = -ballVY;
-    IncrementBallColor();
-  }  
 }
 
 void Scaled4xBlit()
@@ -126,15 +67,17 @@ void Scaled4xBlit()
 
 void DrawToStaging()
 {
-  // Clear the background to white
-  for (int i=0; i<LCD_1IN69_WIDTH * LCD_1IN69_HEIGHT; ++i )
-  {
-    gStaging[i] = WHITE;
-  }
-  
   // Copy from the test image to ministaging
   {
-    unsigned short* pSrc = test_image_1_data;
+    unsigned short* pSrc;
+    if (gImageIndex == 0)
+    {
+      pSrc = test_image_1_data;
+    }
+    else if (gImageIndex == 1)
+    {
+      pSrc = test_image_2_data;
+    }
     unsigned short* pDst = gMiniStaging;
     size_t copySize = test_image_1_width * test_image_1_height * 2;    
     memcpy(pDst, pSrc, copySize);
@@ -151,17 +94,23 @@ void CopyStagingToScreen()
   DEV_SPI_Write_nByte((uint8_t*)&gStaging, LCD_1IN69_WIDTH * LCD_1IN69_HEIGHT * 2);
 }
 
+void IncrementImageIndex()
+{
+  gImageIndex = (gImageIndex+1) % 2;
+}
+
 // the loop function runs over and over again forever
 void loop()
 {
-  MoveBall();
   DrawToStaging();
   CopyStagingToScreen();
 
-  if (!gpio_get(BUTTON1_PIN)) 
+  bool buttonPress = !gpio_get(BUTTON1_PIN);
+  if (gButtonPress == false && buttonPress == true)
   {
-      IncrementBallColor();
-  }
+      IncrementImageIndex();
+  }  
+  gButtonPress = buttonPress;
   
   delay(50); // Wait this number of ms
 }
